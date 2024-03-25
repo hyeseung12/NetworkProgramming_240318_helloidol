e229efe45ba3d2ad7cea5752df58b9fd6dd71ea0
MsEmily1020 <peace10200khs@gmail.com>
2024-03-25 12:16:50 +0900

    views -> templates에 context 전달

--- playground/views.py
+++ playground/views.py
@@ -5,7 +5,11 @@ def say_hello(request):
     return HttpResponse("Hello World")
 
 def say_hello_html(request):
-    return render(request, 'playground/hello.html')
+    return render(request, 'playground/hello.html', {'name': '지우야', 'greeting': '안녀엉~'})
 
 def say_bye_html(request):
-    return render(request, 'playground/bye.html')
+    context = {
+        'singer': '박화요비',
+        'title': '그런 일은',
+    }
+    return render(request, 'playground/bye.html', context=context)
